a1ca380fbbe7989803edcb4ce24cf5fa2c268f18
adastr96 <adam19960@op.pl>
2021-04-06 13:46:06 +0200

    add try catch

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const getDuplicatePosts = require('./src/utils/getDuplicatePosts');
 const getClosestUsers = require('./src/utils/getClosestUsers');
 
 const app = async () => {
-    const users = await getUsers().then(response => response.data);
-    const posts = await getPosts().then(response => response.data);
-
-    console.log(getUsersPosts(users, posts));
-    console.log(getDuplicatePosts(posts));
-    console.log(getClosestUsers(users));
+    try{
+        const users = await getUsers().then(response => response.data);
+        const posts = await getPosts().then(response => response.data);
+    
+        console.log(getUsersPosts(users, posts));
+        console.log(getDuplicatePosts(posts));
+        console.log(getClosestUsers(users));
+    }
+    catch(error) {
+        console.log(error)
+    }
 }
 
 app();
